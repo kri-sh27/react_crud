d34247aedd33e0da33f85e2266eaf550efbdc244
kri-sh27 <hogalekrishna2000@gmail.com>
2023-10-11 12:53:23 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Crud Application</h1>
+      <input type='text' name='movie name'></input>
+    </div>
+  );
+}
+
+export default App;
